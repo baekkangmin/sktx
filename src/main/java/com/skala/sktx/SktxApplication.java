e3fa46c62d15c5ac1d946c32d8a1c904fa347652
벡강민 <rkdgur1902@naver.com>
2026-01-31 11:47:39 +0900

    sktx 프로젝트 신규 생성

---
+++ src/main/java/com/skala/sktx/SktxApplication.java
@@ -0,0 +1,13 @@
+package com.skala.sktx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SktxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SktxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skala/sktx/SktxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skala.sktx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SktxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
